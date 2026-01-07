515f56bd7aaaf98559ae4342c971d3fa23bf88ec
pooja-yadav-py <mispooju@gmail.com>
2026-01-07 13:24:46 +0530

    new changes

--- src/pages/auth/Auth.tsx
+++ src/pages/auth/Auth.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import TermsModal from '@/components/TermsModal'
